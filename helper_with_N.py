71f6e80b9b3ba8970774ef11ee6f56d8233400b8
Jacob Porter <jastuper@gmail.com>
2020-02-10 17:37:20 -0500

    Eliminated quadratic batch creation process. Changed style.

--- helper_with_N.py
+++ helper_with_N.py
@@ -1,27 +1,45 @@
-import numpy as np
-from sklearn.metrics import roc_auc_score
+import sys
+from collections import defaultdict
+
 import keras
+import numpy as np
 from keras.callbacks import Callback
+from sklearn.metrics import roc_auc_score
 
+PROJECT_NAMES = []
 
-PROJECT_NAMES=[]
+options_onehot = defaultdict(lambda: [0, 0, 0, 0, 1])
+
+options_onehot['A'] = [1, 0, 0, 0, 0]
+options_onehot['C'] = [0, 1, 0, 0, 0]
+options_onehot['G'] = [0, 0, 1, 0, 0]
+options_onehot['T'] = [0, 0, 0, 1, 0]
+
 
 # processes an entire dataset of DNA strings to onehot
 def DNA_to_onehot_dataset(dataset):
-  options_onehot = {'A': [1,0,0,0,0],'C' :[0,1,0,0,0], 'G':[0,0,1,0,0] ,'T':[0,0,0,1,0],'N':[0,0,0,0,1]}
-  onehot_data = []
-  for row in dataset:
-    onehot_data.append([options_onehot[e] for e in row])
-  onehot_data = np.array(onehot_data)
-  print(np.shape(onehot_data))
-  return onehot_data 
+    onehot_data = []
+    for row in dataset:
+        try:
+            onehot_data.append([options_onehot[e] for e in row])
+        except KeyError:
+            print(row, file=sys.stdout)
+            raise KeyError
+    onehot_data = np.array(onehot_data)
+    print(np.shape(onehot_data))
+    return onehot_data
+
 
 # processes a DNA string to onehot
 def DNA_to_onehot(dna_line):
-  options_onehot = {'A': [1,0,0,0,0],'C' :[0,1,0,0,0], 'G':[0,0,1,0,0] ,'T':[0,0,0,1,0],'N':[0,0,0,0,1]}
-  onehot_data = [options_onehot[e] for e in dna_line]
-  onehot_data = np.array(onehot_data)
-  return onehot_data 
+    try:
+        onehot_data = [options_onehot[e] for e in dna_line]
+    except KeyError:
+        print(dna_line, file=sys.stdout)
+        raise KeyError
+    onehot_data = np.array(onehot_data)
+    return onehot_data
+
 
 # takes in a line from dataset in format "seq_ID,sequence,label"
 # removes seq_ID, but remembers unique project names
@@ -29,109 +47,127 @@ def process_line(line):
     pieces = line.split(",")
     seq = pieces[1]
     label = int(pieces[2])
-    
-    proj = pieces[0][pieces[0].find("_"):].rstrip("1234567890")
-    if proj not in PROJECT_NAMES:
-        PROJECT_NAMES.append(proj)
+
+    # proj = pieces[0][pieces[0].find("_"):].rstrip("1234567890")
+    # if proj not in PROJECT_NAMES:
+    #     PROJECT_NAMES.append(proj)
     return seq, label
 
+
 # function to take in a dataset and predict for each line
-def predict_one_by_one(model, seqs,labels):
-    prediction_data=np.empty([len(seqs),4]) # true_class, pred_class, proba, seq_length 
-    for i,seq in enumerate(seqs):
-      p = model.predict(np.expand_dims(seq,axis=0))
-      prediction_data[i,0] = labels[i]
-      prediction_data[i,1] = int(p>0.5) 
-      prediction_data[i,2] = p
-      prediction_data[i,3] = len(seq)
+def predict_one_by_one(model, seqs, labels):
+    prediction_data = np.empty([len(seqs), 4
+                                ])  # true_class, pred_class, proba, seq_length
+    for i, seq in enumerate(seqs):
+        p = model.predict(np.expand_dims(seq, axis=0))
+        prediction_data[i, 0] = labels[i]
+        prediction_data[i, 1] = int(p > 0.5)
+        prediction_data[i, 2] = p
+        prediction_data[i, 3] = len(seq)
     return prediction_data
 
 
 # reads in sequences from file, expects them to be 300 bp long,
 # yields a batch of them
-def generate_batches_from_file(path,batch_size):
-    total_counter=0
+def generate_batches_from_file(path, batch_size):
+    total_counter = 0
     for_counter = 0
     submitted_counter = 0
     last_seq = ""
-    print("generator uses b_size: ", batch_size)
+    print("generator uses b_size: ", batch_size, file=sys.stderr)
+    sys.stderr.flush()
 
     while 1:
-        seqs=[]
-        labels=[]
-        batch_counter=0 # counts from 0 to batch_size
-        
-        print("opened file again")
+        seqs = []
+        labels = []
+        batch_counter = 0  # counts from 0 to batch_size
+
+        print("opened file again", file=sys.stderr)
+        sys.stderr.flush()
+
         f = open(path)
         for_counter += 1
 
         for line in f:
             total_counter += 1
-            line = line[:-1] #remove \n
-            
-            seq, lab = process_line(line) # we remove seq_ID
-            assert len(seq)==300, "sequence length should be fixed to 300!, but is "+str(len(seq))+" at line"+str(total_counter)
-            
+            line = line[:-1]  #remove \n
+
+            seq, lab = process_line(line)  # we remove seq_ID
+            assert len(
+                seq
+            ) == 100, "sequence length should be fixed to 100!, but is " + str(
+                len(seq)) + " at line" + str(total_counter)
+
             #just a double-check that we moved to next line
             assert not (seq == last_seq), str(seq)
             last_seq = seq
-        
+
             seqs.append(DNA_to_onehot(seq))
             labels.append(lab)
-            batch_counter+=1
-            if batch_counter==batch_size:
+            batch_counter += 1
+            if batch_counter == batch_size:
                 submitted_counter += batch_size
 
                 yield (np.array(seqs), np.array(labels))
-                batch_counter=0
-                seqs=[]
-                labels=[]
-        print("Did all lines in file ",  submitted_counter, total_counter, for_counter)
-        print("Unique project names", PROJECT_NAMES)
+                batch_counter = 0
+                seqs = []
+                labels = []
+        print("Did all lines in file ",
+              submitted_counter,
+              total_counter,
+              for_counter,
+              file=sys.stderr)
+        sys.stderr.flush()
+        print("Unique project names", PROJECT_NAMES, file=sys.stderr)
+        sys.stderr.flush()
 
-        seqs=[]
-        labels=[]
-        batch_counter=0
-        f.close() # close the file so we can reopen it and loop again.
+        seqs = []
+        labels = []
+        batch_counter = 0
+        f.close()  # close the file so we can reopen it and loop again.
+
 
 #############################################################
 ################## SPECIALIZED AUROC CALLBACKS ##############
 #############################################################
+
 
 # Callback to printout VALIDATION AUROC on each epoch end
 class roc_callback(keras.callbacks.Callback):
     """# Arguments
        validation_data: validation data in format (val_x,val_y)
     """
-    def __init__(self,validation_data):
+    def __init__(self, validation_data):
         self.x_val = validation_data[0]
-        self.y_val = validation_data[1]        
-    
+        self.y_val = validation_data[1]
+
     def on_train_begin(self, logs={}):
         return
- 
+
     def on_train_end(self, logs={}):
         return
- 
+
     def on_epoch_begin(self, epoch, logs={}):
         return
- 
-    def on_epoch_end(self, epoch, logs={}):           
-        
+
+    def on_epoch_end(self, epoch, logs={}):
+
         y_pred_val = self.model.predict(self.x_val)
-        roc_val = roc_auc_score(self.y_val, y_pred_val)      
-        
-        print(" AUROC on Validation: ", str(round(roc_val,4)))
-        if roc_val ==0.5: #happens if learning has crashed
-          self.model.stop_training = True
-          print("ATTENTION: Stopped learning process, becuase learning had in all probability crashed!")
+        roc_val = roc_auc_score(self.y_val, y_pred_val)
+
+        print(" AUROC on Validation: ", str(round(roc_val, 4)))
+        if roc_val == 0.5:  #happens if learning has crashed
+            self.model.stop_training = True
+            print(
+                "ATTENTION: Stopped learning process, becuase learning had in all probability crashed!"
+            )
         return
- 
+
     def on_batch_begin(self, batch, logs={}):
         return
- 
+
     def on_batch_end(self, batch, logs={}):
-        return   
+        return
 
 
 # The following Callbakc allows to only save best models according to VAL AUROC
@@ -157,16 +193,18 @@ class ModelCheckpointAUROC(Callback):
             is saved (`model.save(filepath)`).
         period: Interval (number of epochs) between checkpoints.
     """
-
-    def __init__(self, filepath, validation_data, verbose=0,
-                 save_best_only=False, save_weights_only=False,
+    def __init__(self,
+                 filepath,
+                 validation_data,
+                 verbose=0,
+                 save_best_only=False,
+                 save_weights_only=False,
                  period=1):
         super(ModelCheckpointAUROC, self).__init__()
-        
+
         #added these two
         self.x_val = validation_data[0]
         self.y_val = validation_data[1]
-
 
         self.verbose = verbose
         self.filepath = filepath
@@ -175,14 +213,12 @@ class ModelCheckpointAUROC(Callback):
         self.period = period
         self.epochs_since_last_save = 0
 
-
         # AUROC - the more the better
         self.monitor_op = np.greater
         self.best = -np.Inf
 
-
     def on_epoch_end(self, epoch, logs=None):
-        #calculate AUROC 
+        #calculate AUROC
         y_pred_val = self.model.predict(self.x_val)
         roc_val = roc_auc_score(self.y_val, y_pred_val)
 
@@ -194,15 +230,17 @@ class ModelCheckpointAUROC(Callback):
             if self.save_best_only:
                 current = roc_val
                 if current is None:
-                    warnings.warn('Can save best model. Current area_under_ROC is None.'
-                                  'skipping.', RuntimeWarning)
+                    warnings.warn(
+                        'Can save best model. Current area_under_ROC is None.'
+                        'skipping.', RuntimeWarning)
                 else:
                     if self.monitor_op(current, self.best):
                         if self.verbose > 0:
-                            print(('\nEpoch %05d: %s improved from %0.5f to %0.5f,'
-                                  ' saving model to %s'
-                                  % (epoch + 1, "AUROC", self.best,
-                                     current, filepath)))
+                            print((
+                                '\nEpoch %05d: %s improved from %0.5f to %0.5f,'
+                                ' saving model to %s' %
+                                (epoch + 1, "AUROC", self.best, current,
+                                 filepath)))
                         self.best = current
                         if self.save_weights_only:
                             self.model.save_weights(filepath, overwrite=True)
@@ -210,11 +248,13 @@ class ModelCheckpointAUROC(Callback):
                             self.model.save(filepath, overwrite=True)
                     else:
                         if self.verbose > 0:
-                            print(('\nEpoch %05d: %s did not improve from %0.5f (current: %0.5f)' %
-                                  (epoch + 1, "AUROC", self.best, current)))
+                            print((
+                                '\nEpoch %05d: %s did not improve from %0.5f (current: %0.5f)'
+                                % (epoch + 1, "AUROC", self.best, current)))
             else:
                 if self.verbose > 0:
-                    print(('\nEpoch %05d: saving model to %s' % (epoch + 1, filepath)))
+                    print(('\nEpoch %05d: saving model to %s' %
+                           (epoch + 1, filepath)))
                 if self.save_weights_only:
                     self.model.save_weights(filepath, overwrite=True)
                 else:
@@ -227,7 +267,7 @@ class EarlyStoppingAUROC(Callback):
     """Stop training when a monitored quantity has stopped improving.
     # Arguments
         validation_data: (NEWLY ADDED for AUROC calculation) validation
-            data in format (val_x,val_y)        
+            data in format (val_x,val_y)
         min_delta: minimum change in the monitored quantity
             to qualify as an improvement, i.e. an absolute
             change of less than min_delta, will count as no
@@ -243,8 +283,10 @@ class EarlyStoppingAUROC(Callback):
             If False, the model weights obtained at the last step of
             training are used.
     """
+
     # added validation data as input to the callback
-    def __init__(self,validation_data,
+    def __init__(self,
+                 validation_data,
                  min_delta=0,
                  patience=0,
                  verbose=0,
@@ -253,7 +295,7 @@ class EarlyStoppingAUROC(Callback):
         super(EarlyStoppingAUROC, self).__init__()
 
         self.x_val = validation_data[0]
-        self.y_val = validation_data[1]  
+        self.y_val = validation_data[1]
 
         self.baseline = baseline
         self.patience = patience
@@ -267,7 +309,6 @@ class EarlyStoppingAUROC(Callback):
         # Can hardcode these as metric is defined to be AUROC
         self.monitor_op = np.greater
         self.min_delta *= 1
-
 
     def on_train_begin(self, logs=None):
         # Allow instances to be re-used
@@ -283,7 +324,7 @@ class EarlyStoppingAUROC(Callback):
         y_pred_val = self.model.predict(self.x_val)
         roc_val = roc_auc_score(self.y_val, y_pred_val)
 
-        current = roc_val # everything below is unchanged
+        current = roc_val  # everything below is unchanged
         if current is None:
             return
 
@@ -306,5 +347,3 @@ class EarlyStoppingAUROC(Callback):
     def on_train_end(self, logs=None):
         if self.stopped_epoch > 0 and self.verbose > 0:
             print(('Epoch %05d: early stopping' % (self.stopped_epoch + 1)))
-
-
